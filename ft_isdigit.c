a388baf2a39dd02566678d5c6a048d7822ac0140
dcastor <dcastor@student.42.fr>
2025-04-23 16:48:55 +0200

    is_digit done

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,21 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/23 16:26:23 by dcastor           #+#    #+#             */
-/*   Updated: 2025/04/23 16:47:01 by dcastor          ###   ########.fr       */
+/*   Created: 2025/04/23 16:47:18 by dcastor           #+#    #+#             */
+/*   Updated: 2025/04/23 16:48:38 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int	ft_isalpha(int c)
+int	ft_isdigit(int c)
 {
-	const bool	is_upper = (c >= 'a' && c <= 'z');
-	const bool	is_lower = (c >= 'A' && c <= 'Z');
-
-	return (is_upper || is_lower);
+	return (c >= '0' && c <= '9');
 }
--- libft.h
+++ libft.h
@@ -6,13 +6,16 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 16:13:30 by dcastor           #+#    #+#             */
-/*   Updated: 2025/04/23 16:35:29 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/04/23 16:46:51 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	isalpha(int c);
+# include <stdbool.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
 
 #endif
