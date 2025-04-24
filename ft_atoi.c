9ba7a67f2fbf0ab1e96568c47c441902e08db290
dcastor <dcastor@student.42.fr>
2025-04-24 14:20:13 +0200

    required(atoi): done

---
+++ ft_atoi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 13:49:54 by dcastor           #+#    #+#             */
+/*   Updated: 2025/04/24 14:19:54 by dcastor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int		sign;
+	char	*ptr;
+	int		res;
+
+	ptr = ft_trimstart(nptr);
+	sign = 1;
+	if (ptr == '-' || ptr == '+')
+		if (*ptr++ == '-')
+			sign = -1;
+	while (*ptr && ft_isdigit(*ptr))
+	{
+		res = res * 10 + (*ptr - '0');
+		ptr++;
+	}
+	return (res * sign);
+}
