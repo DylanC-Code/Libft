ff0c0d478fd8616cd86f217b2d26dd647e1cb58b
dcastor <dcastor@student.42.fr>
2025-04-24 13:28:16 +0200

    required(strncmp): fix casting

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:07:44 by dcastor           #+#    #+#             */
-/*   Updated: 2025/04/24 13:14:22 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/04/24 13:26:43 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && s1 && s2)
 	{
-		if (s1[i] || s2[i] && s1[i] != s2[i])
+		if ((s1[i] || s2[i]) && s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
