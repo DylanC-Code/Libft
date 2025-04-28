b20efbc51da025e7987654cd95afbec5d2fba782
dcastor <dcastor@student.42.fr>
2025-04-28 10:50:20 +0200

    bonus(lstadd_back): implemented

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,29 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_bonus.h                                      :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/23 16:13:30 by dcastor           #+#    #+#             */
-/*   Updated: 2025/04/28 10:42:28 by dcastor          ###   ########.fr       */
+/*   Created: 2025/04/28 10:42:50 by dcastor           #+#    #+#             */
+/*   Updated: 2025/04/28 10:48:08 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_BONUS_H
-# define LIBFT_BONUS_H
+#include "libft.h"
 
-# include "libft.h"
-# include <limits.h>
-# include <stdbool.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*actual;
 
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-
-#endif
+	actual = *lst;
+	if (!actual)
+	{
+		*lst = new;
+		return ;
+	}
+	while (actual->next)
+		actual = actual->next;
+	actual->next = new;
+}
